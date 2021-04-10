2f3683148e9faef727f93ae861a5b761e31464f4
asponomarev <82184898+asponomarev@users.noreply.github.com>
2021-04-11 02:25:48 +0300

    v_2021_04_05
    Started working on creating GUI

--- UHLNoCS/MainForm.Designer.cs
+++ UHLNoCS/MainForm.Designer.cs
@@ -28,90 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.StartSimulationButton = new System.Windows.Forms.Button();
-            this.ProgramPathTextBox = new System.Windows.Forms.TextBox();
-            this.ProgramPathLabel = new System.Windows.Forms.Label();
-            this.ProgramArgumentsLabel = new System.Windows.Forms.Label();
-            this.ProgramArgumentsTextBox = new System.Windows.Forms.TextBox();
             this.LogsTextBox = new System.Windows.Forms.TextBox();
-            this.ResultsWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.TestButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // StartSimulationButton
-            // 
-            this.StartSimulationButton.Location = new System.Drawing.Point(154, 138);
-            this.StartSimulationButton.Name = "StartSimulationButton";
-            this.StartSimulationButton.Size = new System.Drawing.Size(120, 30);
-            this.StartSimulationButton.TabIndex = 0;
-            this.StartSimulationButton.Text = "Start simulation";
-            this.StartSimulationButton.UseVisualStyleBackColor = true;
-            this.StartSimulationButton.Click += new System.EventHandler(this.StartSimulationButton_Click);
-            // 
-            // ProgramPathTextBox
-            // 
-            this.ProgramPathTextBox.Location = new System.Drawing.Point(14, 33);
-            this.ProgramPathTextBox.Name = "ProgramPathTextBox";
-            this.ProgramPathTextBox.ReadOnly = true;
-            this.ProgramPathTextBox.Size = new System.Drawing.Size(400, 20);
-            this.ProgramPathTextBox.TabIndex = 1;
-            this.ProgramPathTextBox.DoubleClick += new System.EventHandler(this.ProgramPathTextBox_DoubleClick);
-            // 
-            // ProgramPathLabel
-            // 
-            this.ProgramPathLabel.AutoSize = true;
-            this.ProgramPathLabel.Location = new System.Drawing.Point(14, 17);
-            this.ProgramPathLabel.Name = "ProgramPathLabel";
-            this.ProgramPathLabel.Size = new System.Drawing.Size(143, 13);
-            this.ProgramPathLabel.TabIndex = 2;
-            this.ProgramPathLabel.Text = "Path to model executable file";
-            // 
-            // ProgramArgumentsLabel
-            // 
-            this.ProgramArgumentsLabel.AutoSize = true;
-            this.ProgramArgumentsLabel.Location = new System.Drawing.Point(14, 73);
-            this.ProgramArgumentsLabel.Name = "ProgramArgumentsLabel";
-            this.ProgramArgumentsLabel.Size = new System.Drawing.Size(88, 13);
-            this.ProgramArgumentsLabel.TabIndex = 3;
-            this.ProgramArgumentsLabel.Text = "Model arguments";
-            // 
-            // ProgramArgumentsTextBox
-            // 
-            this.ProgramArgumentsTextBox.Location = new System.Drawing.Point(14, 89);
-            this.ProgramArgumentsTextBox.Name = "ProgramArgumentsTextBox";
-            this.ProgramArgumentsTextBox.Size = new System.Drawing.Size(400, 20);
-            this.ProgramArgumentsTextBox.TabIndex = 4;
-            this.ProgramArgumentsTextBox.DoubleClick += new System.EventHandler(this.ProgramArgumentsTextBox_DoubleClick);
             // 
             // LogsTextBox
             // 
             this.LogsTextBox.AcceptsReturn = true;
-            this.LogsTextBox.Location = new System.Drawing.Point(14, 199);
+            this.LogsTextBox.Location = new System.Drawing.Point(12, 12);
             this.LogsTextBox.Multiline = true;
             this.LogsTextBox.Name = "LogsTextBox";
             this.LogsTextBox.ReadOnly = true;
             this.LogsTextBox.Size = new System.Drawing.Size(400, 250);
             this.LogsTextBox.TabIndex = 5;
             // 
-            // ResultsWebBrowser
+            // TestButton
             // 
-            this.ResultsWebBrowser.Location = new System.Drawing.Point(472, 33);
-            this.ResultsWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.ResultsWebBrowser.Name = "ResultsWebBrowser";
-            this.ResultsWebBrowser.Size = new System.Drawing.Size(500, 416);
-            this.ResultsWebBrowser.TabIndex = 6;
+            this.TestButton.Location = new System.Drawing.Point(135, 299);
+            this.TestButton.Name = "TestButton";
+            this.TestButton.Size = new System.Drawing.Size(133, 23);
+            this.TestButton.TabIndex = 6;
+            this.TestButton.Text = "Run";
+            this.TestButton.UseVisualStyleBackColor = true;
+            this.TestButton.Click += new System.EventHandler(this.TestButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.ResultsWebBrowser);
+            this.Controls.Add(this.TestButton);
             this.Controls.Add(this.LogsTextBox);
-            this.Controls.Add(this.ProgramArgumentsTextBox);
-            this.Controls.Add(this.ProgramArgumentsLabel);
-            this.Controls.Add(this.ProgramPathLabel);
-            this.Controls.Add(this.ProgramPathTextBox);
-            this.Controls.Add(this.StartSimulationButton);
             this.Name = "MainForm";
             this.Text = "UHLNoCS";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -121,14 +68,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button StartSimulationButton;
-        private System.Windows.Forms.TextBox ProgramPathTextBox;
-        private System.Windows.Forms.Label ProgramPathLabel;
-        private System.Windows.Forms.Label ProgramArgumentsLabel;
-        private System.Windows.Forms.TextBox ProgramArgumentsTextBox;
         private System.Windows.Forms.TextBox LogsTextBox;
-        private System.Windows.Forms.WebBrowser ResultsWebBrowser;
+        private System.Windows.Forms.Button TestButton;
     }
 }
 
--- UHLNoCS/MainForm.cs
+++ UHLNoCS/MainForm.cs
@@ -7,30 +7,16 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UHLNoCS.Models;
 using UHLNoCS.Topologies;
 
 namespace UHLNoCS
 {
     public partial class MainForm : Form
     {
-        public static string CurrentTopology;
-
-        public static string CurrentTopologyArgument1;
-        public static string CurrentTopologyArgument2;
-        public static string CurrentTopologyArgument3;
-
-        public static string CurrentAlgorithm;
-
-        public static string CurrentAlgorithmArgument;
-
-        public static string CurrentXmlPath;
-
-        public static string TopologyDescription;
-        public static string TopologyNetlist;
-        public static string TopologyRouting;
-
         public MainForm()
         {
             InitializeComponent();
@@ -38,106 +24,7 @@ namespace UHLNoCS
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            ResetParameters();
-        }
-
-        private void StartSimulationButton_Click(object sender, EventArgs e)
-        {
-            ResetParameters();
-            LogsTextBox.Clear();
-
-            string ProgramPath = ProgramPathTextBox.Text;
-            PrepareArguments(ProgramArgumentsTextBox.Text);
-            string ProgramArguments = CurrentXmlPath;
-
-            Process Proc = new Process();
-            Proc.StartInfo.FileName = ProgramPath;
-            Proc.StartInfo.Arguments = ProgramArguments;
-            Proc.StartInfo.CreateNoWindow = true;
-
-            try
-            {
-                bool StartedOk = Proc.Start();
-                if (StartedOk)
-                {
-                    ToLogsTextBox(DateTime.Now.ToString());
-                    ToLogsTextBox("Process started");
-                    ToLogsTextBox(ProgramPath);
-                    ToLogsTextBox(ProgramArguments);
-
-                    Proc.WaitForExit();
-                    int ExitCode = Proc.ExitCode;
-
-                    ToLogsTextBox("Process finished with exit code " + ExitCode.ToString());
-
-                    Proc.Close();
-                    //
-                    string ResultPath = Directory.GetCurrentDirectory() + "\\results";
-                    string[] TmpDirs = Directory.GetDirectories(ResultPath);
-
-                    string LastDirName = "";
-                    DateTime LastDateTime = DateTime.MinValue;
-
-                    foreach (string Dir in TmpDirs)
-                    {
-                        if (Directory.GetLastWriteTime(Dir) > LastDateTime)
-                        {
-                            LastDirName = Dir;
-                            LastDateTime = Directory.GetLastWriteTime(Dir);
-                        }
-                    }
-                    //
-                    string[] TmpFiles = Directory.GetFiles(LastDirName);
-
-                    string LastFileName = "";
-                    LastDateTime = DateTime.MinValue;
-
-                    foreach (string FileName in TmpFiles)
-                    {
-                        if (File.GetLastWriteTime(FileName) > LastDateTime && FileName.EndsWith(".html"))
-                        {
-                            LastFileName = FileName;
-                            LastDateTime = File.GetLastWriteTime(FileName);
-                        }
-                    };
-                    //
-                    ResultsWebBrowser.Navigate(LastFileName);
-
-                    string Text = ResultsWebBrowser.DocumentText;
-
-                    Encoding utf8 = Encoding.GetEncoding("UTF-8");
-                    Encoding win1251 = Encoding.GetEncoding("Windows-1251");
-
-                    byte[] utf8Bytes = win1251.GetBytes(Text);
-                    byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
-
-                    ResultsWebBrowser.DocumentText = Text;
-                    ResultsWebBrowser.Refresh();
-                }
-                else
-                {
-                    ToLogsTextBox("Process start failed");
-                }
-            }
-            catch (Exception Ex)
-            {
-                ToLogsTextBox("Error in process");
-                ToLogsTextBox(Ex.Message);
-            }
-        }
-
-        private void ProgramPathTextBox_DoubleClick(object sender, EventArgs e)
-        {
-            using (OpenFileDialog Dialog = new OpenFileDialog())
-            {
-                Dialog.InitialDirectory = Directory.GetCurrentDirectory();
-
-                if (Dialog.ShowDialog() == DialogResult.OK)
-                {
-                    string Path = Dialog.FileName;
-                    ProgramPathTextBox.Text = Path;
-                }
-            }
+                       
         }
 
         private void ToLogsTextBox(string Text)
@@ -152,179 +39,76 @@ namespace UHLNoCS
             }
         }
 
-        private void ProgramArgumentsTextBox_DoubleClick(object sender, EventArgs e)
+        private void TestButton_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog Dialog = new OpenFileDialog())
-            {
-                Dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            //DateTime TestStart = DateTime.Now;
 
-                if (Dialog.ShowDialog() == DialogResult.OK)
-                {
-                    string Path = Dialog.FileName;
-                    ProgramArgumentsTextBox.Text = Path;
-                }
+            ToLogsTextBox("Models creation started");
+
+            string[] args1 = new string[2] { "3", "2" };
+            string[] algs1 = new string[1] { "10" };
+            UOCNS Uocns1 = new UOCNS(ModelsTypes.UOCNS, "UOCNS1", "C:\\Users\\Lenovo\\Desktop\\VKR\\UOCNS\\jar\\UOCNS.jar",
+                                    Directory.GetCurrentDirectory().ToString() + "\\test1", true,
+                                    Directory.GetCurrentDirectory().ToString() + "\\test1\\config.xml",
+                                    TopologiesTypes.Mesh, args1, "ROU", algs1, UOCNS.DefaultFifoSize, UOCNS.DefaultFifoCount,
+                                    UOCNS.DefaultFlitSize, UOCNS.DefaultPacketSizeAvg, UOCNS.DefaultPacketSizeIsFixed,
+                                    UOCNS.DefaultPacketPeriodAvg, UOCNS.DefaultCountRun, UOCNS.DefaultCountPacketRx,
+                                    UOCNS.DefaultCountPacketRxWarmUp, UOCNS.DefaultIsModeGALS);
+
+            string[] args2 = new string[2] { "2", "3" };
+            string[] algs2 = new string[1] { "10" };
+            UOCNS Uocns2 = new UOCNS(ModelsTypes.UOCNS, "UOCNS2", "C:\\Users\\Lenovo\\Desktop\\VKR\\UOCNS\\jar\\UOCNS.jar",
+                                    Directory.GetCurrentDirectory().ToString() + "\\test2", true,
+                                    Directory.GetCurrentDirectory().ToString() + "\\test2\\config.xml",
+                                    TopologiesTypes.Mesh, args2, "ROU", algs2, UOCNS.DefaultFifoSize, UOCNS.DefaultFifoCount,
+                                    UOCNS.DefaultFlitSize, UOCNS.DefaultPacketSizeAvg, UOCNS.DefaultPacketSizeIsFixed,
+                                    UOCNS.DefaultPacketPeriodAvg, UOCNS.DefaultCountRun, UOCNS.DefaultCountPacketRx,
+                                    UOCNS.DefaultCountPacketRxWarmUp, UOCNS.DefaultIsModeGALS);
+
+            ToLogsTextBox("Models creation finished");
+
+            Thread TestThread1 = new Thread(TestThreadTask);
+            TestThread1.IsBackground = true;
+            TestThread1.Start(Uocns1);
+
+            Thread TestThread2 = new Thread(TestThreadTask);
+            TestThread2.IsBackground = true;
+            TestThread2.Start(Uocns2);
+
+            while (TestThread1.IsAlive && TestThread2.IsAlive)
+            {
+                Thread.Sleep(100);
             }
+
+            //DateTime TestEnd = DateTime.Now;
+            //ToLogsTextBox((TestEnd - TestStart).ToString());
         }
 
-        private void PrepareArguments(string RawArguments)
+        public void TestThreadTask(object Model)
         {
-            string[] RawArgumentsParts = RawArguments.Split();
+            UOCNS Uocns = (UOCNS)Model;
 
-            if (RawArgumentsParts.Length == 1)
-            {
-                CurrentXmlPath = RawArgumentsParts[0];
-            }
-            else
-            {
-                CurrentTopology = RawArgumentsParts[0];
+            ToLogsTextBox(Uocns.GetName() + " Simulation thread started");
+            
+            ToLogsTextBox(Uocns.GetName() + " Preparation for simulation started");
+            Uocns.PrepareForSimulation();
+            ToLogsTextBox(Uocns.GetName() + " Preparation for simulation finished");
 
-                if (CurrentTopology == Common.TopologyMesh)
-                {
-                    CurrentTopologyArgument1 = RawArgumentsParts[1];
-                    CurrentTopologyArgument2 = RawArgumentsParts[2];
+            Process Proc = new Process();
+            Proc.StartInfo.FileName = Uocns.GetExecutableFilePath();
+            Proc.StartInfo.Arguments = Uocns.GetConfigFilePath() + " " + Uocns.GetResultsDirectoryPath();
+            Proc.StartInfo.CreateNoWindow = true;
 
-                    Mesh NewMesh = new Mesh(Int32.Parse(CurrentTopologyArgument1), Int32.Parse(CurrentTopologyArgument2));
-                    NewMesh.CreateNetlist();
-                    NewMesh.CreateRouting();
+            Proc.Start();
+            ToLogsTextBox(Uocns.GetName() + " Simulation process started");
 
-                    TopologyNetlist = MatrixToString(NewMesh.GetNetlist());
-                    TopologyRouting = MatrixToString(NewMesh.GetRouting());
-                    TopologyDescription = CurrentTopology + "-(" + CurrentTopologyArgument1 + ", " + CurrentTopologyArgument2 + ")";
+            Proc.WaitForExit();
+            int ExitCode = Proc.ExitCode;
+            ToLogsTextBox(Uocns.GetName() + " Simulation process finished with exit code " + ExitCode.ToString());
 
-                    Common.CreateXml(CurrentXmlPath, TopologyDescription, TopologyNetlist, TopologyRouting);
-                }
+            Proc.Close();
 
-                if (CurrentTopology == Common.TopologyTorus)
-                {
-                    CurrentTopologyArgument1 = RawArgumentsParts[1];
-                    CurrentTopologyArgument2 = RawArgumentsParts[2];
-
-                    Torus NewTorus = new Torus(Int32.Parse(CurrentTopologyArgument1), Int32.Parse(CurrentTopologyArgument2));
-                    NewTorus.CreateNetlist();
-                    NewTorus.CreateRouting();
-
-                    TopologyNetlist = MatrixToString(NewTorus.GetNetlist());
-                    TopologyRouting = MatrixToString(NewTorus.GetRouting());
-                    TopologyDescription = CurrentTopology + "-(" + CurrentTopologyArgument1 + ", " + CurrentTopologyArgument2 + ")";
-
-                    Common.CreateXml(CurrentXmlPath, TopologyDescription, TopologyNetlist, TopologyRouting);
-                }
-
-                if (CurrentTopology == Common.TopologyCirculant)
-                {
-                    CurrentTopologyArgument1 = RawArgumentsParts[1];
-                    CurrentTopologyArgument2 = RawArgumentsParts[2];
-                    CurrentTopologyArgument3 = RawArgumentsParts[3];
-
-                    if (RawArgumentsParts.Length == 4)
-                    {
-                        CurrentAlgorithm = Common.AlgorithmDijkstra;
-                    }
-                    else
-                    {
-                        CurrentAlgorithm = RawArgumentsParts[4];
-                        if (CurrentAlgorithm == Common.AlgorithmROU)
-                        {
-                            if (RawArgumentsParts.Length == 5)
-                            {
-                                CurrentAlgorithmArgument = "10";
-                            }
-                            else
-                            {
-                                CurrentAlgorithmArgument = RawArgumentsParts[5];
-                            }
-                        }
-                    }
-
-                    Circulant NewCirculant = new Circulant(Int32.Parse(CurrentTopologyArgument1), Int32.Parse(CurrentTopologyArgument2), Int32.Parse(CurrentTopologyArgument3));
-                    NewCirculant.CreateNetlist();
-                    NewCirculant.CreateRouting(NewCirculant.AdjacencyMatrix(NewCirculant.GetNetlist(), Int32.Parse(CurrentTopologyArgument1)), NewCirculant.GetNetlist());
-
-                    TopologyNetlist = MatrixToString(NewCirculant.GetNetlist());
-                    TopologyRouting = MatrixToString(NewCirculant.GetRouting());
-                    TopologyDescription = CurrentTopology + "-(" + CurrentTopologyArgument1 + ", " + CurrentTopologyArgument2 + ", " + CurrentTopologyArgument3 + ")";
-
-                    Common.CreateXml(CurrentXmlPath, TopologyDescription, TopologyNetlist, TopologyRouting);
-                }
-
-                if (CurrentTopology == Common.TopologyOptimalCirculant)
-                {
-                    CurrentTopologyArgument1 = RawArgumentsParts[1];
-
-                    if (RawArgumentsParts.Length == 2)
-                    {
-                        CurrentAlgorithm = Common.AlgorithmDijkstra;
-                    }
-                    else
-                    {
-                        CurrentAlgorithm = RawArgumentsParts[2];
-                        if (CurrentAlgorithm == Common.AlgorithmROU)
-                        {
-                            if (RawArgumentsParts.Length == 3)
-                            {
-                                CurrentAlgorithmArgument = "10";
-                            }
-                            else
-                            {
-                                CurrentAlgorithmArgument = RawArgumentsParts[4];
-                            }
-                        }
-                    }
-
-                    Circulant NewCirculant = new Circulant(Int32.Parse(CurrentTopologyArgument1));
-                    NewCirculant.CreateNetlist();
-                    NewCirculant.CreateRouting(NewCirculant.AdjacencyMatrix(NewCirculant.GetNetlist(), Int32.Parse(CurrentTopologyArgument1)), NewCirculant.GetNetlist());
-
-                    TopologyNetlist = MatrixToString(NewCirculant.GetNetlist());
-                    TopologyRouting = MatrixToString(NewCirculant.GetRouting());
-                    TopologyDescription = CurrentTopology + "-(" + CurrentTopologyArgument1 + ", " + NewCirculant.S1.ToString() + ", " + NewCirculant.S2.ToString() + ")";
-
-                    Common.CreateXml(CurrentXmlPath, TopologyDescription, TopologyNetlist, TopologyRouting);
-                }
-
-            }
-        }
-
-        private string MatrixToString(int[,] Matrix)
-        {
-            string Result = "\r\n";
-
-            int Rows = Matrix.GetLength(0);
-            int Cols = Matrix.GetLength(1);
-
-            for (int Row = 0; Row < Rows; Row++)
-            {
-                for (int Col = 0; Col < Cols; Col++)
-                {
-                    Result += Matrix[Row, Col].ToString();
-                    if (Col != Cols - 1)
-                    {
-                        Result += " ";
-                    }
-                }
-                Result += "\r\n";
-            }
-
-            return Result;
-        }
-
-        private void ResetParameters()
-        {
-            CurrentTopology = "";
-
-            CurrentTopologyArgument1 = "";
-            CurrentTopologyArgument2 = "";
-            CurrentTopologyArgument3 = "";
-
-            CurrentAlgorithm = "";
-
-            CurrentAlgorithmArgument = "";
-
-            CurrentXmlPath = Directory.GetCurrentDirectory() + "\\results\\LastSimulation\\config.xml";
-
-            TopologyDescription = "";
-            TopologyNetlist = "";
-            TopologyRouting = "";
+            ToLogsTextBox(Uocns.GetName() + " Simulation thread finished");
         }
 
     }
